f9ba4728971ec4fe5771afbd1377ab6d3e0c9c85
Gurung <agurung@calstatela.edu>
2022-03-18 16:43:37 -0700

    Configure react app for development to github pages

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,38 @@
+import { motion, AnimatePresence } from 'framer-motion'
+import { useContext } from 'react'
+import FeedbackItem from "./FeedbackItem"
+import FeedbackContext from '../context/FeedbackContext'
+
+function FeedbackList() {
+    const {feedback} = useContext(FeedbackContext)
+
+    if(!feedback || feedback.length === 0){
+        return <p>No Feedback Yet</p>
+    }
+    return (
+        <div className="feedback-list">
+            <AnimatePresence>
+            <div>{feedback.map((item)=>(
+                <motion.div 
+                key={item.id}
+                initial={{opacity: 0}}
+                animate={{opacity : 1}}
+                exit={{opacity: 0}}>
+                <FeedbackItem key={item.id} item={item} />
+                </motion.div>
+            ))}</div>
+            </AnimatePresence>
+        </div>
+      )
+//     return (
+//     <div className="feedback-list">
+//         <div>{feedback.map((item)=>(
+//             <FeedbackItem key={item.id} item={item} handleDelete = {handleDelete} />
+//         ))}</div>
+//     </div>
+//   )
+}   
+
+
+
+export default FeedbackList
